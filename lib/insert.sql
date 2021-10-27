19d59fef96f7d33a7ff6f9aecf1f0b9ede9db015
Catherine Loesch <catherine.loesch@protonmail.com>
2021-10-27 01:18:00 +0100

    assignment completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr Chocolate", 3, "M", "brown", "choleric", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M", "light brown", "rowdy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "feisty", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 11, "M", "black", "sensitive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "light brown", "melancholic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9, "M", "brown", "optimistic", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "black", "impatient", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 13, "F", "dark brown", "calm", 1);
